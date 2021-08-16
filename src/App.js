4960dd49a463d3b36c7ed05497f90eb7bfb8c27e
Jacob Marthaller <marthaller.jr@gmail.com>
2021-08-16 07:02:08 -0700

    Hook components up to App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { NavBar } from "./components/NavBar";
+import { About } from "./components/About";
+import { Projects } from "./components/Projects";
+import { Experience } from "./components/Experience";
+import { Contact } from "./components/Contact";
 
 export default function App() {
   return (
-      <NavBar />
+    <div>
+        <NavBar />
+        <About />
+        <Projects />
+        <Experience />
+        <Contact />
+    </div>
     );
 }
