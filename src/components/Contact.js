2c81b2b61ac57270359bffdb0684b63360c718d6
Jacob Marthaller <marthaller.jr@gmail.com>
2021-08-20 07:05:43 -0700

    Initial styling to NavBar

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-export default function Contact() {
+export function Contact() {
 
 
     const [name, setName] = useState("");
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,7 @@
 import { ArrowRightIcon } from "@heroicons/react/solid";
 import React from "react";
 
-export default function Navbar() {
+export  function NavBar() {
   return (
     <header className="bg-gray-800 md:sticky top-0 z-10">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
