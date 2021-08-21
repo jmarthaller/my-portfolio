4e1a6c2fbd068052b7fb507530fc3bc6fb77d9d1
Jacob Marthaller <marthaller.jr@gmail.com>
2021-08-21 04:15:00 -0700

    Fix bug in App main tag

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { NavBar } from "./components/NavBar";
 import { About } from "./components/About";
 import { Projects } from "./components/Projects";
@@ -6,19 +6,10 @@ import { Experience } from "./components/Experience";
 import { Contact } from "./components/Contact";
 
 export default function App() {
-  const [count, setCount] = useState(0)
-
-  useEffect(() => {
-
-    const handleCount = () => {
-      for (let i = 0; i < 100000; i++) { console.log(1) }
-      setCount(count + 1)
-    }
-    handleCount()
-  }, [count])
+  
 
   return (
-     <main main className="text-gray-400 bg-gray-900 body-font">
+     <main className="text-gray-400 bg-gray-900 body-font">
         <NavBar />
         <About />
         <Projects />
