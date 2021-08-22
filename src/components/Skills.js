4c213b2168a04f0c2ee83cf8981c29c7e5b09047
Jacob Marthaller <marthaller.jr@gmail.com>
2021-08-22 11:46:50 -0700

    Re-center Skills section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,15 +4,17 @@ import React from "react";
 export function Skills() {
   return (
     <section id="skills">
-      <div className="container px-5 py-10 mx-autod">
-        <div className="text-center mb-20">
-          <ChipIcon className="w-10 inline-block mb-4" />
-          <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-4">
-            Skills &amp; Technologies
-          </h1>
-          <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
-            Placeholder text
-          </p>
+      <div className="container px-5 py-10 mx-auto">
+        <div className="container px-5 py-10 mx-auto">
+          <div id="skills" className="text-center mb-20">
+            <ChipIcon className="w-10 inline-block mb-4" />
+            <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-4">
+              Skills &amp; Technologies
+            </h1>
+            <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
+              Placeholder text
+            </p>
+          </div>
         </div>
       </div>
     </section>
