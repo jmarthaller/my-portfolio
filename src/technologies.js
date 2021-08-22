176a91db860be058104124c60b21681496025f8b
Jacob Marthaller <marthaller.jr@gmail.com>
2021-08-22 12:30:03 -0700

    Add Language logos

---
+++ src/technologies.js
@@ -0,0 +1,62 @@
+export const technologies = [
+  {
+    name: "HTML",
+    image:
+      "https://icongr.am/devicon/html5-plain.svg?size=128&color=9ca3af",
+  },
+  {
+    name: "CSS",
+    image:
+      "https://icongr.am/devicon/css3-plain.svg?size=128&color=9ca3af",
+  },
+  {
+    name: "JavaScript",
+    image:
+      "https://icongr.am/devicon/javascript-plain.svg?size=50&color=9ca3af",
+  },
+  {
+    name: "React",
+    image:
+      "https://icongr.am/devicon/react-original.svg?size=50&color=9ca3af",
+  },
+  {
+    name: "TypeScript",
+    image:
+      "https://icongr.am/simple/typescript.svg?size=50&color=9ca3af",
+  },
+  {
+    name: "Node",
+    image:
+      "https://icongr.am/devicon/nodejs-plain.svg?size=50&color=9ca3af",
+  },
+  {
+    name: "Ruby",
+    image:
+      "https://icongr.am/devicon/ruby-plain.svg?size=50&color=9ca3af",
+  },
+  {
+    name: "Rails",
+    image:
+      "https://icongr.am/devicon/rails-plain.svg?size=50&color=9ca3af",
+  },
+  {
+    name: "Git",
+    image:
+      "https://icongr.am/devicon/git-plain.svg?size=50&color=9ca3af",
+  },
+  {
+    name: "PostgreSQL",
+    image:
+      "https://icongr.am/devicon/postgresql-plain.svg?size=50&color=9ca3af",
+  },
+  {
+    name: "Docker",
+    image:
+      "https://icongr.am/devicon/docker-plain.svg?size=50&color=9ca3af",
+  },
+  {
+    name: "GraphQL",
+    image:
+      "https://icongr.am/simple/apollographql.svg?size=50&color=9ca3af",
+  },
+];
