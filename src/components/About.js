dd2fd06dba95bef7eee75e21cf26713aa173d779
Jacob Marthaller <marthaller.jr@gmail.com>
2021-08-23 06:04:55 -0700

    Add emoji to intro paragraph

--- src/components/About.js
+++ src/components/About.js
@@ -6,27 +6,35 @@ export function About() {
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-            Hi, I'm Jacob
+            Hi, I'm Jacob{" "}
+            <span role="img" aria-label="waving hand emjoi">
+              👋
+            </span>
             <br className="hidden lg:inline-block" />I love to build amazing
             software
           </h1>
           <p className="mb-8 leading-relaxed">
-          I'm a Full Stack Software Engineer working primarily in JavaScript, React.js, and Ruby on Rails. 
-          I love creating software because it allows me to express myself  creatively while solving hard problems.
-          <br />
-          <br />
-          Before getting into web development I worked in legal services, where I narrowly avoided becoming an attorney.
-          When I'm not progamming I'm usually writing on Medium, surfing, or cooking something French (read: lots of butter).
+            I'm a Full Stack Software Engineer working primarily in JavaScript,
+            React.js, and Ruby on Rails. I love creating software because it
+            allows me to express myself creatively while solving hard problems.
+            <br />
+            <br />
+            Before getting into web development I worked in legal services,
+            where I narrowly avoided becoming an attorney. When I'm not
+            progamming I'm usually writing on Medium, surfing, or cooking
+            something French (read: lots of butter).
           </p>
           <div className="flex justify-center">
             <a
               href="#contact"
-              className="inline-flex text-white bg-green-700 border-0 py-2 px-6 focus:outline-none hover:bg-green-800 rounded text-lg">
+              className="inline-flex text-white bg-green-700 border-0 py-2 px-6 focus:outline-none hover:bg-green-800 rounded text-lg"
+            >
               Work With Me
             </a>
             <a
               href="#projects"
-              className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
+              className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg"
+            >
               See My Past Work
             </a>
           </div>
--- src/technologies.js
+++ src/technologies.js
@@ -1,13 +1,11 @@
 export const technologies = [
   {
     name: "HTML",
-    image:
-      "https://icongr.am/devicon/html5-plain.svg?size=128&color=9ca3af",
+    image: "https://icongr.am/devicon/html5-plain.svg?size=128&color=9ca3af",
   },
   {
     name: "CSS",
-    image:
-      "https://icongr.am/devicon/css3-plain.svg?size=128&color=9ca3af",
+    image: "https://icongr.am/devicon/css3-plain.svg?size=128&color=9ca3af",
   },
   {
     name: "JavaScript",
@@ -16,33 +14,27 @@ export const technologies = [
   },
   {
     name: "React",
-    image:
-      "https://icongr.am/simple/react.svg?size=50&color=9ca3af",
+    image: "https://icongr.am/simple/react.svg?size=50&color=9ca3af",
   },
   {
     name: "TypeScript",
-    image:
-      "https://icongr.am/simple/typescript.svg?size=50&color=9ca3af",
+    image: "https://icongr.am/simple/typescript.svg?size=50&color=9ca3af",
   },
   {
     name: "Node",
-    image:
-      "https://icongr.am/devicon/nodejs-plain.svg?size=50&color=9ca3af",
+    image: "https://icongr.am/devicon/nodejs-plain.svg?size=50&color=9ca3af",
   },
   {
     name: "Ruby",
-    image:
-      "https://icongr.am/devicon/ruby-plain.svg?size=50&color=9ca3af",
+    image: "https://icongr.am/devicon/ruby-plain.svg?size=50&color=9ca3af",
   },
   {
     name: "Rails",
-    image:
-      "https://icongr.am/devicon/rails-plain.svg?size=50&color=9ca3af",
+    image: "https://icongr.am/devicon/rails-plain.svg?size=50&color=9ca3af",
   },
   {
     name: "Git",
-    image:
-      "https://icongr.am/devicon/git-plain.svg?size=50&color=9ca3af",
+    image: "https://icongr.am/devicon/git-plain.svg?size=50&color=9ca3af",
   },
   {
     name: "PostgreSQL",
@@ -50,24 +42,19 @@ export const technologies = [
       "https://icongr.am/devicon/postgresql-plain.svg?size=50&color=9ca3af",
   },
   {
-    name: "Docker",
-    image:
-      "https://icongr.am/devicon/docker-plain.svg?size=50&color=9ca3af",
-  },
-  {
     name: "MongoDB",
-    image:
-      "https://icongr.am/simple/mongodb.svg?size=50&color=9ca3af",
+    image: "https://icongr.am/simple/mongodb.svg?size=50&color=9ca3af",
   },
   {
     name: "Redis",
-    image:
-      "https://icongr.am/simple/redis.svg?size=50&color=9ca3af",
+    image: "https://icongr.am/simple/redis.svg?size=50&color=9ca3af",
   },
   {
-    name: "Jest",
-    image:
-      "https://icongr.am/simple/jest.svg?size=50&color=9ca3af",
+    name: "Docker",
+    image: "https://icongr.am/devicon/docker-plain.svg?size=50&color=9ca3af",
   },
-
+  {
+    name: "GraphQL",
+    image: "https://icongr.am/simple/graphql.svg?size=50&color=9ca3af",
+  },
 ];
