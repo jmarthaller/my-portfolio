25c1a56f18eef5f87ba00510de0e3a98a71c6511
Jacob Marthaller <marthaller.jr@gmail.com>
2021-08-30 06:34:12 -0700

    Adjust imports on App.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import { NavBar } from "./components/NavBar";
 import { About } from "./components/About";
 import { Projects } from "./components/Projects";
 import { Experience } from "./components/Experience";
+import { Skills } from "./components/Skills";
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
-import { Skills } from "./components/Skills";
 
 export default function App() {
   return (
