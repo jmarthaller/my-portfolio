01f571f3ae195166d1bcd1c488af0a4d84c0589c
Jacob Marthaller <marthaller.jr@gmail.com>
2021-09-02 13:19:01 -0700

    Add social media icons to About - not refined

--- src/components/About.js
+++ src/components/About.js
@@ -47,6 +47,64 @@ export function About() {
             </span>
             something French (read: lots of butter).
           </p>
+          {/* start here */}
+          <div className="flex justify-center">
+          <a
+            href="https://www.linkedin.com/in/jacob-marthaller/"
+            className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none rounded text-base mt-2 md:mt-0"
+          >
+            <img
+              align="left"
+              alt="linkedin-logo"
+              src="https://icongr.am/fontawesome/linkedin-square.svg?size=128&color=9099a5"
+              height="30"
+              width="30"
+            />
+          </a>
+          <a
+            href="https://github.com/jmarthaller"
+            className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none rounded text-base mt-2 md:mt-0"
+          >
+            <img
+              align="left"
+              alt="github-logo"
+              src="https://icongr.am/fontawesome/github.svg?size=128&color=9099a5"
+              height="30"
+              width="30"
+            />
+          </a>
+          <a
+            href="https://twitter.com/jmartNYC"
+            className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none rounded text-base mt-2 md:mt-0"
+          >
+            <img
+              align="left"
+              alt="twitter-logo"
+              src="https://icongr.am/simple/twitter.svg?size=128&color=9099a5"
+              height="30"
+              width="30"
+            />
+          </a>
+          <a
+            href="https://marthaller-jr.medium.com/"
+            className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none rounded text-base mt-2 md:mt-0"
+          >
+            <img
+              align="left"
+              alt="medium-logo"
+              src="https://icongr.am/fontawesome/medium.svg?size=128&color=9099a5"
+              height="30"
+              width="30"
+            />
+          </a>
+
+
+          </div>
+
+
+
+
+        {/* end here */}
           <div className="flex justify-center">
             <a
               href="#contact"
