d535d99b628b6afb3696694c36413350081bcb05
Jacob Marthaller <marthaller.jr@gmail.com>
2021-09-03 08:31:53 -0700

    Ensure NavBar on top of hover elements with z-index

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@ import resume from "../resume.pdf";
 
 export function NavBar() {
   return (
-    <header className="bg-gray-800 md:sticky top-0 z-10">
+    <header className="bg-gray-800 md:sticky top-0 z-40">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <a className="title-font font-medium text-white mb-4 md:mb-0">
           <a href="" className="ml-3 text-xl">
