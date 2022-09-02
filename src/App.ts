44302c17aa6c55ada6f5c0f69778660da0a34650
Jacob Marthaller <marthaller.jr@gmail.com>
2022-09-02 12:09:52 -0700

    Format App.ts

--- src/App.ts
+++ src/App.ts
@@ -1,22 +1,22 @@
-import React from "react";
-import { NavBar } from "./components/NavBar";
-import { About } from "./components/About";
-import { Projects } from "./components/Projects";
-import { Experience } from "./components/Experience";
-import { Skills } from "./components/Skills";
-import { Contact } from "./components/Contact";
-import { Footer } from "./components/Footer";
+// import React from "react";
+// import { NavBar } from "./components/NavBar";
+// import { About } from "./components/About";
+// import { Projects } from "./components/Projects";
+// import { Experience } from "./components/Experience";
+// import { Skills } from "./components/Skills";
+// import { Contact } from "./components/Contact";
+// import { Footer } from "./components/Footer";
 
-export default function App() {
-  return (
-    <main className="text-gray-400 bg-gray-900 body-font">
-      <NavBar />
-      <About />
-      <Projects />
-      <Experience />
-      <Skills />
-      <Contact />
-      <Footer />
-    </main>
-  );
-}
+// export default function App() {
+//   return (
+//     <main className="text-gray-400 bg-gray-900 body-font">
+//       <NavBar />
+//       <About />
+//       <Projects />
+//       <Experience />
+//       <Skills />
+//       <Contact />
+//       <Footer />
+//     </main>
+//   );
+// }
