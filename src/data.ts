e1fa43937cbf64bfc369e84084a5aeed8366cda2
Jacob Marthaller <marthaller.jr@gmail.com>
2022-09-22 09:52:11 -0700

    Type adjustment in data.ts

--- src/data.ts
+++ src/data.ts
@@ -1,50 +1,93 @@
-// export const projects = [
-//     {
-//       title: "Sleuth Files",
-//       subtitle: "React + TypeScript, Node.js",
-//       description:
-//         "A Reddit-inspired communication app that allows users to share information about real-life true crime events",
-//       image: "sleuth_files.png",  
-//       link: "https://github.com/nselvidge/case-files",
-//     },
-//     {
-//       title: "Putrid Potatoes",
-//       subtitle: "React and Ruby on Rails",
-//       description:
-//         "A LetterBoxd clone that allows users to log the films they've watched, write reviews of those films, and interact with other users on the platform",
-//       image: "login-potato.gif",  
-//       link: "https://putridpotatoes.netlify.app/",
-//     },
-//     {
-//       title: "FinTeck",
-//       subtitle: "React + TypeScript, Node.js",
-//       description:
-//         "A site to aggregate predictive data to forecast the likelihood of human-shark interaction off the coast of California's most popular beaches",
-//       image: "finteck-mock.png",  
-//       link: "https://github.com/jmarthaller/finteck",
-//     },
-//     {
-//       title: "Polenta To Go Around",
-//       subtitle: "React and Ruby on Rails",
-//       description:
-//         "A website designed to fight food waste by linking users with restaraunts seeking to sell discounted food that would otherwise be discarded",
-//       image: "polenta-favorite.gif",
-//       link: "https://polenta-to-go-around.netlify.app/",
-//     },
-//     {
-//       title: "YouTravel",
-//       subtitle: "Vanilla JavaScript and Ruby on Rails",
-//       description:
-//         "A YouTube-inspired video application that allows users to preview vacation destinations by watching videos detailing major global cities",
-//       image: "yt-login.gif",
-//       link: "https://youtravel.netlify.app/",
-//     },
-//     {
-//       title: "HomeFIT",
-//       subtitle: "Ruby",
-//       description:
-//         "A command-line Ruby application that facilitates workouts by randomly selecting body weight exercises for the user to perform at home",
-//       image: "command_line.gif",
-//       link: "https://github.com/maifam/ruby-project-alt-guidelines",
-//     },
-//   ];
+export const projects: [
+  {
+    title: string;
+    subtitle: string;
+    description: string;
+    image: string;
+    link: string;
+  },
+  {
+    title: string;
+    subtitle: string;
+    description: string;
+    image: string;
+    link: string;
+  },
+  {
+    title: string;
+    subtitle: string;
+    description: string;
+    image: string;
+    link: string;
+  },
+  {
+    title: string;
+    subtitle: string;
+    description: string;
+    image: string;
+    link: string;
+  },
+  {
+    title: string;
+    subtitle: string;
+    description: string;
+    image: string;
+    link: string;
+  },
+  {
+    title: string;
+    subtitle: string;
+    description: string;
+    image: string;
+    link: string;
+  }
+] = [
+  {
+    title: "Sleuth Files",
+    subtitle: "React + TypeScript, Node.js",
+    description:
+      "A Reddit-inspired communication app that allows users to share information about real-life true crime events",
+    image: "sleuth_files.png",
+    link: "https://github.com/nselvidge/case-files",
+  },
+  {
+    title: "Putrid Potatoes",
+    subtitle: "React and Ruby on Rails",
+    description:
+      "A LetterBoxd clone that allows users to log the films they've watched, write reviews of those films, and interact with other users on the platform",
+    image: "login-potato.gif",
+    link: "https://putridpotatoes.netlify.app/",
+  },
+  {
+    title: "FinTeck",
+    subtitle: "React + TypeScript, Node.js",
+    description:
+      "A site to aggregate predictive data to forecast the likelihood of human-shark interaction off the coast of California's most popular beaches",
+    image: "finteck-mock.png",
+    link: "https://github.com/jmarthaller/finteck",
+  },
+  {
+    title: "Polenta To Go Around",
+    subtitle: "React and Ruby on Rails",
+    description:
+      "A website designed to fight food waste by linking users with restaraunts seeking to sell discounted food that would otherwise be discarded",
+    image: "polenta-favorite.gif",
+    link: "https://polenta-to-go-around.netlify.app/",
+  },
+  {
+    title: "YouTravel",
+    subtitle: "Vanilla JavaScript and Ruby on Rails",
+    description:
+      "A YouTube-inspired video application that allows users to preview vacation destinations by watching videos detailing major global cities",
+    image: "yt-login.gif",
+    link: "https://youtravel.netlify.app/",
+  },
+  {
+    title: "HomeFIT",
+    subtitle: "Ruby",
+    description:
+      "A command-line Ruby application that facilitates workouts by randomly selecting body weight exercises for the user to perform at home",
+    image: "command_line.gif",
+    link: "https://github.com/maifam/ruby-project-alt-guidelines",
+  },
+];
