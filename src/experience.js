cb1fda3bec6fd38001ed5882d741f1dc8b91e92e
Jacob Marthaller <marthaller.jr@gmail.com>
2022-11-07 20:00:00 -0600

    testing time

--- src/experience.js
+++ src/experience.js
@@ -11,7 +11,7 @@ export const experience = [
     company: "Hearth.io",
     year: "Mar 2021-Sep 2021",
     companyLink: "https://hearth.io/",
-    desc: "Refactored multiple components TypeScript | Leveraged GraphQL to describe and fetch data throughout application | Enhanced mobile web experience by making several pages mobile-responsive | Conducted market research with potential users",
+    desc: "Refactored multiple components  | Leveraged GraphQL to describe and fetch data throughout application | Enhanced mobile web experience by making several pages mobile-responsive | Conducted market research with potential users",
   },
   {
     title: "Digital Court Reporter/Technical Writer",
