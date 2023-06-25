b560201c71fa4af5c637d32dcdb0d58c6306b784
Jacob Marthaller <marthaller.jr@gmail.com>
2023-06-25 12:35:12 -0700

    Add Costar to experience array

--- dist/experience.js
+++ dist/experience.js
@@ -2,32 +2,44 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.experience = void 0;
 exports.experience = [
-    {
-        title: "Software Engineer",
-        company: "Olive AI",
-        year: "Sep 2021-Now",
-        companyLink: "https://oliveai.com/",
-        desc: "Built internal applications to enhance the productivity of Olive’s automation engineers | Designed web and API-based automations in TypeScript to process thousands of patients’ insurance coverage daily | Developed a React desktop application to streamline electronic health record documentation",
-    },
-    {
-        title: "Software Engineer",
-        company: "Hearth.io",
-        year: "Mar 2021-Sep 2021",
-        companyLink: "https://hearth.io/",
-        desc: "Migrated codebases from JavaScript to TypeScript | Leveraged GraphQL to describe and fetch data throughout application | Enhanced mobile web experience by making several pages mobile-responsive | Conducted market research with potential users",
-    },
-    {
-        title: "Digital Court Reporter/Technical Writer",
-        company: "Neal R. Gross & Co, Inc.",
-        year: "2016-2020",
-        companyLink: "http://nealrgross.com/",
-        desc: "Designed FOIA request-response pipeline to optimize bid success rate by 25% | Recorded and transcribed 300+ criminal and civil legal proceedings | Drafted policy-driven technical reports",
-    },
-    {
-        title: "Chief Operations Officer",
-        company: "Taylored Productions",
-        year: "2012-2013",
-        companyLink: "",
-        desc: "Launched a video production company and oversaw operations for over two dozen clients | Implemented accounting procedures that resulted in a budget surplus | Supervised and trained 5 person team of apprentice editors",
-    },
+  {
+    title: "Software Engineer",
+    company: "CoStar Group (Apartments.com)",
+    year: "December 2022-Now",
+    companyLink: "https://www.costargroup.com/",
+    desc:
+      "Built Tax Document edelivery | Lease Renewal work | Facilitated documentation efforts",
+  },
+  {
+    title: "Software Engineer",
+    company: "Olive AI",
+    year: "Sep 2021-Oct 2022",
+    companyLink: "https://oliveai.com/",
+    desc:
+      "Built internal applications to enhance the productivity of Olive’s automation engineers | Designed web and API-based automations in TypeScript to process thousands of patients’ insurance coverage daily | Developed a React desktop application to streamline electronic health record documentation",
+  },
+  {
+    title: "Software Engineer",
+    company: "Hearth.io",
+    year: "Mar 2021-Sep 2021",
+    companyLink: "https://hearth.io/",
+    desc:
+      "Migrated codebases from JavaScript to TypeScript | Leveraged GraphQL to describe and fetch data throughout application | Enhanced mobile web experience by making several pages mobile-responsive | Conducted market research with potential users",
+  },
+  {
+    title: "Digital Court Reporter/Technical Writer",
+    company: "Neal R. Gross & Co, Inc.",
+    year: "2016-2020",
+    companyLink: "http://nealrgross.com/",
+    desc:
+      "Designed FOIA request-response pipeline to optimize bid success rate by 25% | Recorded and transcribed 300+ criminal and civil legal proceedings | Drafted policy-driven technical reports",
+  },
+  {
+    title: "Chief Operations Officer",
+    company: "Taylored Productions",
+    year: "2012-2013",
+    companyLink: "",
+    desc:
+      "Launched a video production company and oversaw operations for over two dozen clients | Implemented accounting procedures that resulted in a budget surplus | Supervised and trained 5 person team of apprentice editors",
+  },
 ];
